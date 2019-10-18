a682cdd2a171c27772b182ed863148d3fdbcc221
Joshua Reno <jralphur@gmail.com>
2019-10-18 15:50:58 -0700

    2.9 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,34 +2,56 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [persons, addPersons] = useState([
-    {name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
 
   const [newName, setNewName] = useState('');
-
+  const [newNumber, setNewNumber] = useState('');
+  const [filterResults, setFilter] = useState('');
   const handleSubmit = (event) => {
     event.preventDefault()
     if (persons.findIndex(p => p.name.toUpperCase() === newName.toUpperCase()) === -1) {
       const nextPerson = {
         name: newName,
+        number: newNumber
       }
       addPersons(persons.concat(nextPerson));
+      setNewName("");
+      setNewNumber("");
     }
     else alert(`${newName} is already in the phonebook`)
     
-    setNewName("");
   }
 
-  const handleStateChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with 
+        <input onChange={handleFilterChange} value={filterResults} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit} >
         <div>
-          name: <input onChange={handleStateChange} value={newName} ></input>
+          name: <input onChange={handleNameChange} value={newName} ></input>
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} value={newNumber}></input>
         </div>
         <div>
           <button type="submit">add</button>
@@ -37,7 +59,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}> {person.name} </li>)}
+        {persons.filter(
+          person => person.name.toLowerCase().includes(filterResults.toLowerCase()))
+          .map(person => <li key={person.name}> {person.name} {person.number}</li>)}
       </ul>
     </div>
   )
