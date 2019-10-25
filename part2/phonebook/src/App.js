ffc8f6ffd77e7c79c7227db085fd272ca7272c43
Joshua Reno <jralphur@gmail.com>
2019-10-25 15:20:36 -0700

    2.20 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
 import Persons from './components/Persons';
+import Status from './components/Status';
 import personService from './services/phonebook.js'
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
   const [filterResults, setFilter] = useState('');
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [error, setError] = useState({message: "", class: ""});
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -31,8 +33,10 @@ const App = () => {
       setNewName("")
       setNewNumber("")
     }).catch((error) => {
-      alert(`${person.name} is already deleted, or doesn't exist`)
+      setError({message: `${person.name} is already deleted, or doesn't exist`, class: "error"})
       addPersons(persons.filter(person => person.id !== id))
+
+      setTimeout(() => setError({message: "", class: ""}), 5000);
     })
   }
 }
@@ -44,32 +48,35 @@ const App = () => {
       number: newNumber
   }
     if (personIndex === -1) {
-     
-
         personService.addPerson(nextPerson)
             .then(newPerson => {
                 addPersons(persons.concat(newPerson));
                 setNewName("");
                 setNewNumber("");
         }).catch(reason => {
-          alert(reason)
+          setError({message: reason, error: "error"});
+          setTimeout(() => setError({message: "", class: ""}), 5000);
         })
     }
     else {
       const {name, id} = persons[personIndex];
       if (window.confirm(`${name} is already in the phonebook, replace the old number with a new one?`)) {
-        personService.update(id, nextPerson).then(newPerson => 
+        personService.update(id, nextPerson).then(newPerson => {
           addPersons(persons.map(person => person.id !== id ? person : newPerson))
+          setError({message: `Successfully updated ${name} into the database`, class: "success"})
+          setTimeout(() => setError({message: "", class: ""}), 10000000000000000);
+        }
         )
       }
     }
   }
 
-
   useEffect(() => { personService.getAll().then(persons => addPersons(persons)) } 
            ,[]);
+
   return (
     <div>
+      <Status className={error.class} message={error.message} />
       <h1>Phonebook</h1>
       { /* filter */ }
       <Filter filterResults={filterResults} setFilter={setFilter} />
